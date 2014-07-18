1a28c3c178f5e477567bc51e25e0f8f410a42d3e
cjwd <chinarakinda@gmail.com>
2014-07-18 01:39:29 -0400

    Add styles for contact form

--- resources/js/myfolio.js
+++ resources/js/myfolio.js
@@ -10,7 +10,7 @@ define([
         init: function() {
             this.stickyNav();
             this.scroll();
-            this.spmenu();
+            this.contactForm();
         },
 
         stickyNav: function(){
@@ -20,6 +20,51 @@ define([
         scroll: function(){
             $('.site-nav a').smoothScroll();
         },
+
+        contactForm: function() {
+            $.support.placeholder = function(){
+                var input = document.createElement('input');
+                return 'placeholder' in input;
+            }
+
+            if($.support.placeholder) {
+                var $li = $('.form-fields li');
+
+                $li.each(function(){
+                    $(this).addClass('js-hide-label');
+                });
+
+                $li.find('input, textarea').on('keyup blur focus', function(e){
+            
+                    var $this = $(this),
+                        $parent = $this.parent();
+
+                    if (e.type == 'keyup') {
+                        if( $this.val() == '' ) {
+                            $parent.addClass('js-hide-label'); 
+                        } else {
+                            $parent.removeClass('js-hide-label');   
+                        }                     
+                    } 
+                    else if (e.type == 'blur') {
+                        if( $this.val() == '' ) {
+                            $parent.addClass('js-hide-label');
+                        } 
+                        else {
+                            $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
+                        }
+                    } 
+                    else if (e.type == 'focus') {
+                        if( $this.val() !== '' ) {
+                            $parent.removeClass('js-unhighlight-label');
+                        }
+                    }
+                });
+
+
+            }
+
+        }
 
         /*spmenu: function(){
             var menuRight = document.getElementById( 'spmenu-right' ),
